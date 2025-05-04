8e093b7eb6d49a2516f9f276d32d36d931469ac1
Dave Caraway <dwcaraway@gmail.com>
2025-05-03 20:28:41 -0400

    have question selection and next question buttons working

--- screens/quiz_screen.py
+++ screens/quiz_screen.py
@@ -39,25 +39,37 @@ class QuizScreen(Screen):
     class QuizScreenCanvas(FloatLayout):
         def on_kv_post(self, base_widget):
             super().on_kv_post(base_widget)
-            self.load_next_QuestionAnswerSet()
+            self.load_next_question_answer_set()
 
-        def on_raindrop_select(self, instance):
-            instance.md_bg_color = [0, 1, 0, 1]  # Glowing border effect
+        # def on_raindrop_select(self, instance):
+        #     instance.md_bg_color = [0, 1, 0, 1]  # Glowing border effect
 
         def submit_answer(self, answer: int) -> None:
             """
             Submits an answer
 
-            answer: int - The 0-based index of the answer
+            answer: int - The 0-based index of the answer button clickedq
             """
+            # TODO set level to debug
+            Logger.info(f"Answer submitted: {answer}")
             
-            Logger.debug(f"Answer submitted: {answer}")
-            
-        def load_next_QuestionAnswerSet(self) -> None:
+        def load_next_question_answer_set(self) -> None:
             """
-            Load the next QuestionAnswerSet in the quiz.
+            Load the next a question and set of answers in the quiz.
             """
-            Logger.debug("Loading next QuestionAnswerSet...")
+            # TODO set level to debug
+            Logger.info("Loading next QuestionAnswerSet...")
+            # Pick a random QuestionAnswerSet from the list
             self.question_answer_set = choice(question_answer_sets)
-            # Logic to load the next QuestionAnswerSet goes here
+
+            # TODO these lookups are aweful! try out namespace search
+            answer_button_group = self.children[0].children[0].children[1].children
+            question_label = self.children[0].children[0].children[2].children[0]
+
+            # Update the question_label with the new question
+            question_label.text = self.question_answer_set.question
+
+            # Update the answer buttons with the new answers
+            for i, answer in enumerate(self.question_answer_set.answers):
+                answer_button_group[i].text = answer
 
